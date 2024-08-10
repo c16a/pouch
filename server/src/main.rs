e2ed08267de74bcc508d19db8571ed0469ff2cd4
Chaitanya Munukutla <chaitanya.m61292@gmail.com>
2024-08-10 17:21:11 +0530

    Add WebSocket interface
    Signed-off-by: Chaitanya Munukutla <chaitanya.m61292@gmail.com>

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
+use std::fmt::format;
 use std::ops::DerefMut;
 use std::sync::Arc;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 use tokio::net::{TcpListener, TcpStream};
 use tokio::sync::RwLock;
+use tokio_tungstenite::tungstenite::protocol::Message;
+use tokio_tungstenite::accept_async;
+use futures_util::{StreamExt, SinkExt};
 
 use crate::processor::db::InMemoryDb;
 use crate::processor::spec::Processor;
@@ -18,20 +22,8 @@ mod wal;
 
 #[tokio::main]
 async fn main() {
-    // Default values
-    let default_port = 6379;
     let default_wal_file = "wal.log".to_string();
-
-    let port: u16 = env::var("PORT")
-        .ok()
-        .and_then(|p| p.parse().ok())
-        .unwrap_or(default_port);
-
     let wal_file = env::var("WAL_FILE").unwrap_or(default_wal_file);
-
-    let address = format!("127.0.0.1:{}", port);
-    let tcp_listener = TcpListener::bind(&address).await.unwrap();
-
     let wal = Arc::new(RwLock::new(WAL::new(&wal_file).unwrap()));
     let db = Arc::new(RwLock::new(InMemoryDb::new().unwrap()));
 
@@ -48,17 +40,67 @@ async fn main() {
         }
     }
 
-    loop {
-        let (socket, _) = tcp_listener.accept().await.unwrap();
-        let wal = wal.clone();
-        let db = db.clone();
+    let enable_tcp: bool = env::var("ENABLE_TCP").ok().and_then(|p| p.parse().ok()).unwrap_or(true);
+    if enable_tcp {
+        let default_tcp_port = 6379;
+        let tcp_port: u16 = env::var("TCP_PORT")
+            .ok()
+            .and_then(|p| p.parse().ok())
+            .unwrap_or(default_tcp_port);
+
+        let default_tcp_host = String::from("0.0.0.0");
+        let tcp_host = env::var("TCP_HOST").ok().unwrap_or(default_tcp_host);
+        let tcp_address = format!("{}:{}", tcp_host, tcp_port);
+
+        let tcp_listener = TcpListener::bind(&tcp_address).await.unwrap();
+
+        let wal_tcp = wal.clone();
+        let db_tcp = db.clone();
+
         tokio::spawn(async move {
-            process(socket, db, wal).await;
+            format!("started TCP listener on {}", tcp_address);
+            loop {
+                let (socket, _) = tcp_listener.accept().await.unwrap();
+                let wal = wal_tcp.clone();
+                let db = db_tcp.clone();
+                tokio::spawn(async move {
+                    process_tcp(socket, db, wal).await;
+                });
+            }
         });
+    }
+
+    let enable_ws: bool = env::var("ENABLE_WS").ok().and_then(|p| p.parse().ok()).unwrap_or(false);
+    if enable_ws {
+        let default_ws_port = 6389;
+        let ws_port: u16 = env::var("WS_PORT")
+            .ok()
+            .and_then(|p| p.parse().ok())
+            .unwrap_or(default_ws_port);
+
+        let default_ws_host = String::from("0.0.0.0");
+        let ws_host = env::var("WS_HOST").ok().unwrap_or(default_ws_host);
+        let ws_address = format!("{}:{}", ws_host, ws_port);
+        let ws_listener = TcpListener::bind(&ws_address).await.unwrap();
+
+        let wal_ws = wal.clone();
+        let db_ws = db.clone();
+
+        tokio::spawn(async move {
+            format!("started WS listener on {}", ws_address);
+            loop {
+                let (socket, _) = ws_listener.accept().await.unwrap();
+                let wal = wal_ws.clone();
+                let db = db_ws.clone();
+                tokio::spawn(async move {
+                    handle_websocket(socket, db, wal).await;
+                });
+            }
+        }).await.unwrap();
     }
 }
 
-async fn process(mut socket: TcpStream, db: Arc<RwLock<dyn Processor>>, wal: Arc<RwLock<WAL>>) {
+async fn process_tcp(mut socket: TcpStream, db: Arc<RwLock<dyn Processor>>, wal: Arc<RwLock<WAL>>) {
     let mut buf = vec![0; 1024];
 
     loop {
@@ -107,5 +149,52 @@ async fn process(mut socket: TcpStream, db: Arc<RwLock<dyn Processor>>, wal: Arc
         // Clear the buffer for the next read
         buf.clear();
         buf.resize(1024, 0); // Reset the buffer size
+    }
+}
+
+async fn handle_websocket(stream: TcpStream, db: Arc<RwLock<dyn Processor>>, wal: Arc<RwLock<WAL>>) {
+    match accept_async(stream).await {
+        Ok(ws_stream) => {
+            let (mut ws_sender, mut ws_receiver) = ws_stream.split();
+
+            while let Some(msg) = ws_receiver.next().await {
+                match msg {
+                    Ok(msg) => {
+                        if msg.is_text() {
+                            let json_str = msg.into_text().unwrap();
+
+                            let response = match Command::from_json(&json_str) {
+                                Err(err) => {
+                                    eprintln!("error parsing command: {}", err);
+                                    Response::Err {
+                                        error: UnknownCommand,
+                                    }
+                                }
+                                Ok(cmd) => {
+                                    db.write()
+                                        .await
+                                        .cmd(cmd, Some(wal.write().await.deref_mut()))
+                                }
+                            };
+
+                            let json_str = response.to_json().unwrap();
+
+                            if let Err(err) = ws_sender.send(Message::text(json_str)).await {
+                                eprintln!("failed to send WebSocket message; err = {:?}", err);
+                            }
+                        } else if msg.is_close() {
+                            break;
+                        }
+                    }
+                    Err(err) => {
+                        eprintln!("WebSocket error: {:?}", err);
+                        break;
+                    }
+                }
+            }
+        }
+        Err(err) => {
+            eprintln!("Failed to accept WebSocket connection; err = {:?}", err);
+        }
     }
 }
